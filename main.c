68f16d5e771163b87716f1bf7c9a2d4edeec8e34
w00332938 <wangyushui@huawei.com>
2021-05-18 20:45:02 +0800

    first merge

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    if (argc > 1) {
+        printf("argc > 1\n");
+    }
+    else {
+        printf("argc <= 1\n");
+    }
+
+    return 0;
+}
